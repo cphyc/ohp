1b915a1616cff825898639b30251112fcc1bcb1c
ufe local user <localuser@ufe-edport8.obspm.fr>
2016-03-08 02:21:54 +0100

    have io and its position

--- analyse.py
+++ analyse.py
@@ -2,7 +2,7 @@
 import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
-
+plt.ion()
 # Open files
 bg = fits.open('jupiter_08032016_0040_bg')
 jupiter_raw = fits.open('jupiter_08032016_0039')
@@ -13,4 +13,20 @@ bg_med = np.median(bg[0].data, axis=0)
 # Remove background baby
 jupiter = [_data - bg_med for _data in jupiter_raw[0].data]
 
-# 
+# plt.imshow(jupiter[0], interpolation='none')
+
+# Cut around io (man)
+io = [jup[140:180, 50:100] for jup in jupiter]
+
+# Find maximum
+maxis = [np.unravel_index(np.argmax(_io), _io.shape) for _io in io]
+
+# plt.ioff()
+# for i in range(len(maxis)):
+#     plt.cla()
+#     plt.imshow(io[i])
+#     plt.plot(maxis[i][1], maxis[i][0], 'ro')
+#     plt.show()
+io_stacked = np.mean(io, axis=0)
+plt.imshow(io_stacked, interpolation='none')
+plt.savefig('io_stacked.pdf')
