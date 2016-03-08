194d555f0b4476b38ae8f20f01919dbf5943b7ba
ufe local user <localuser@ufe-edport8.obspm.fr>
2016-03-08 03:23:39 +0100

    stacking succesfully
    I love cheeseburgers

--- analyse.py
+++ analyse.py
@@ -2,6 +2,7 @@
 import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
+from astropy.modeling import models, fitting
 plt.ion()
 # Open files
 bg = fits.open('jupiter_08032016_0040_bg')
@@ -16,17 +17,27 @@ jupiter = [_data - bg_med for _data in jupiter_raw[0].data]
 # plt.imshow(jupiter[0], interpolation='none')
 
 # Cut around io (man)
-io = [jup[140:180, 50:100] for jup in jupiter]
+offset = (140, 55)
+io = [jup[offset[0]:offset[0]+40, offset[1]:offset[1]+40] for jup in jupiter]
 
 # Find maximum
-maxis = [np.unravel_index(np.argmax(_io), _io.shape) for _io in io]
+maxis = np.array([np.unravel_index(np.argmax(_io), _io.shape) for _io in io])
+maxis_abs = maxis + np.array(offset)
+minx = np.min(maxis_abs[:,0])
+miny = np.min(maxis_abs[:,1])
+deltax = np.max(maxis_abs[:,0]) - minx 
+deltay = np.max(maxis_abs[:,1]) - miny
 
 # plt.ioff()
-# for i in range(len(maxis)):
+# for i in range(10):#len(maxis)):
 #     plt.cla()
-#     plt.imshow(io[i])
-#     plt.plot(maxis[i][1], maxis[i][0], 'ro')
+#     plt.imshow(jupiter[i])
+#     plt.plot(maxis_abs[i][1], maxis_abs[i][0], 'ro')
 #     plt.show()
-io_stacked = np.mean(io, axis=0)
-plt.imshow(io_stacked, interpolation='none')
-plt.savefig('io_stacked.pdf')
+
+# Big array that contains everything
+stacked = np.zeros(np.array(jupiter[0].shape) + np.array([deltax, deltay]))
+width, height = jupiter[0].shape
+for i in range(len(jupiter)):
+    stacked[maxis_abs[i][0] - minx:maxis_abs[i][0] - minx + width,
+            maxis_abs[i][1] - miny:maxis_abs[i][1] - miny + height] += jupiter[i]
